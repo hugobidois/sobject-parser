2f049cca947eca8accf1152a6e5972154979d573
Hugo Bidois <hugo.bidois@icom-cloud.com>
2022-03-22 17:39:20 +0100

    Add version and help alias

--- index.js
+++ index.js
@@ -3,57 +3,60 @@ const JSON_INDENT = 2;
 const fs = require('fs');
 const yargs = require('yargs/yargs');
 const {hideBin} = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).command(
-  '$0 <source file> [destination file]',
-  'the default command',
-  () => {},
-  ({destinationfile, sourcefile}) => {
-    fs.readFile(sourcefile, 'utf8', (readError, data) => {
-      if (readError) {
-        throw readError;
-      }
-      data = JSON.parse(data);
-      const schema = {
-        additionalProperties: false,
-        properties: {},
-        required: [],
-        type: 'object',
-      };
-      if (data.fields) {
-        for (const field of data.fields) {
-          if (field.createable && field.updateable) {
-            const fieldSchema = {};
-            if (field.nillable === false) {
-              schema.required.push(field.name);
-            }
-            fieldSchema.type = field.type;
-            if (field.type === 'string') {
-              fieldSchema.maxLength = field.length;
-            }
-            if (field.type === 'picklist') {
-              fieldSchema.enum = field.picklistValues.map(
-                picklistValue => picklistValue.value,
-              );
-              if (field.restrictedPicklist === false) {
-                fieldSchema.enum.push('anyValue');
+const argv = yargs(hideBin(process.argv))
+  .alias('v', 'version')
+  .alias('h', 'help')
+  .command(
+    '$0 <source file> [destination file]',
+    'the default command',
+    () => {},
+    ({destinationfile, sourcefile}) => {
+      fs.readFile(sourcefile, 'utf8', (readError, data) => {
+        if (readError) {
+          throw readError;
+        }
+        data = JSON.parse(data);
+        const schema = {
+          additionalProperties: false,
+          properties: {},
+          required: [],
+          type: 'object',
+        };
+        if (data.fields) {
+          for (const field of data.fields) {
+            if (field.createable && field.updateable) {
+              const fieldSchema = {};
+              if (field.nillable === false) {
+                schema.required.push(field.name);
               }
+              fieldSchema.type = field.type;
+              if (field.type === 'string') {
+                fieldSchema.maxLength = field.length;
+              }
+              if (field.type === 'picklist') {
+                fieldSchema.enum = field.picklistValues.map(
+                  picklistValue => picklistValue.value,
+                );
+                if (field.restrictedPicklist === false) {
+                  fieldSchema.enum.push('anyValue');
+                }
+              }
+              schema.properties[field.name] = fieldSchema;
             }
-            schema.properties[field.name] = fieldSchema;
           }
         }
-      }
-      const writeData = JSON.stringify(schema, null, JSON_INDENT);
-      if (destinationfile) {
-        fs.writeFile(destinationfile, writeData, writeError => {
-          if (writeError) {
-            throw writeError;
-          }
-          console.log('File saved!');
-        });
-      }
-    });
-  },
-).argv;
+        const writeData = JSON.stringify(schema, null, JSON_INDENT);
+        if (destinationfile) {
+          fs.writeFile(destinationfile, writeData, writeError => {
+            if (writeError) {
+              throw writeError;
+            }
+            console.log('File saved!');
+          });
+        }
+      });
+    },
+  ).argv;
 
 if (!argv) {
   throw 'No arguments provided';
